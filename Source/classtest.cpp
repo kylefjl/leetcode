87c2083e35e997adfcc52410a792d7bcb9337496
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-02-17 12:03:57 +0800

    main

--- Source/classtest.cpp
+++ Source/classtest.cpp
@@ -1,28 +1,52 @@
-///////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdlib.h>
 #include < vector>
 #include <iostream>
 using namespace std; //命名空间
-#define uint16 unsigned int
+#define uint32 unsigned int
 #define uint8 unsigned char
-
+#define uint16 wchar_t cpp
 class CHero
 {
 public:
     void SetHP(int hp) //设置血量
     {
-        m_HP = hp;
+        this->m_HP = hp;
+		cout << "get hp: " << GetHP() << endl;
     }
     int GetHP() //获得hp
     {
         return m_HP;
+       
     }
-
 protected: //保护
 private:
     int m_HP; //私有,血量
 };
+
+
+class Address//地址
+{
+public:
+	long long  telenum;
+	string one_address;
+};
+
+//信息
+class Person
+{
+public:
+	string name;
+	uint8 age;
+	Address address;
+	Person(string name, uint8 age, Address addr)//构造函数
+	{
+		this->name = name;
+		this->age = age;
+		this->address = addr;
+	}
+};
+
 
 int main()
 {
@@ -34,6 +58,9 @@ int main()
     cout << "hp: " << hero.GetHP() << endl;
     cout << "php :" << phero->GetHP() << endl; //类指针
     printf("hello \n");
+
+    Person person("fjl",21,{12345678910,"shanxi"});//构造函数初始话
+    
     system("pause");
     return 0;
 }
