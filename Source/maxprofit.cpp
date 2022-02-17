fd457184d3696144916759f97fbfbde17f384b14
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-02-17 12:32:18 +0800

    main

--- Source/maxprofit.cpp
+++ Source/maxprofit.cpp
@@ -1,12 +1,17 @@
 
 
-///////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include < vector> 
-using namespace std;
-#define uint16 unsigned int
+#include <iostream>
+using namespace std;//命名空间
+#define uint16 wchar_t
+#define uint32 unsigned int
 #define uint8 unsigned char
+#define int16 short
+
+
 
 /*********************************************************************************
   *Function:       求最大利润
@@ -18,20 +23,32 @@ using namespace std;
   *Return:   
   *Others:    
 **********************************************************************************/
-int max_profit(uint16_t* nums)//去重
-{
-	
-	return nums_long;//返回数据长度
-}
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+		int n=prices.size();
+		int maxp=0;
+		if (n==0)//数组为空返回
+		{
+			return 0;
+		}
+		for(int i=0;i<n-1;i++)
+		{
+			prices[i]=prices[i+1]-prices[i];//求插值
+			if(prices[i]>=0)//累加正值
+			{
+				maxp+=prices[i];
+			}
+		}
+		return maxp;//返回最大利润
+    }
+};
 int main()
-{
-	int nums[10]={1,2,3,4,4,4,4,7,8,9};
-	uint16 numlong = 0;	
-	unsigned char size;
-	size = sizeof(nums)/4;
-	numlong=similar_remove(nums, size);
-	printf("long:%d\n",numlong);
+{	
+	vector<int> nums={1,2,3,4,4,4,4,7,8,9};//初始化数组，对应10天的股票值
+	Solution solution;
+	int maxp=solution.maxProfit(nums);
+	printf("max profit %d\n",maxp);
 	system("pause");
 	return 0;
 }
