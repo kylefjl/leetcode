f6a2d8c2670c5313668dd5da2107600d87856b09
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-02-18 11:15:58 +0800

    main

---
+++ Source/containsDuplicate.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include < vector> 
+#include <iostream>
+using namespace std;//命名空间
+#define uint16 wchar_t
+#define uint32 unsigned int
+#define uint8 unsigned char
+#define int16 short
+/*********************************************************************************
+  *Function:     
+  *Description： 
+  *Calls:        
+  *Called By:    
+  *Input:        
+  *Output:       
+  *Return:       
+  *Others:       
+**********************************************************************************/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+    }
+};
+int main()
+{	
+    printf("hello");
+    system("pause");
+    return 0;
+}
