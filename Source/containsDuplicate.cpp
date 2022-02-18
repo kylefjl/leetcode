6f9046cff052039133344aa56d163a160995f788
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-02-18 11:22:00 +0800

    main

--- Source/containsDuplicate.cpp
+++ Source/containsDuplicate.cpp
@@ -8,19 +8,20 @@ using namespace std;//命名空间
 #define uint8 unsigned char
 #define int16 short
 /*********************************************************************************
-  *Function:     
-  *Description： 
+  *Function:     查询重复数组
+  *Description： 1：有重复 0：无重复
   *Calls:        
   *Called By:    
-  *Input:        
+  *Input:        数组
   *Output:       
-  *Return:       
-  *Others:       
+  *Return:       1：有重复 0：无重复
+  *Others:       先排序，再比较
 **********************************************************************************/
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        
     }
 };
 int main()
