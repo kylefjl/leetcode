3857d9499e320eaaa735a8cb03c5dff69cea87fc
kylefjl <3219029834@mail.nwpu.edu.cn>
2023-01-15 13:00:11 +0800

    完成程序部分

--- editor/cn/P24_SwapNodesInPairs.cpp
+++ editor/cn/P24_SwapNodesInPairs.cpp
@@ -42,7 +42,13 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 /*
 * 两两交换链表中的节点
 * @author name
@@ -62,13 +68,71 @@ using namespace std;
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
+        auto*  head_pre=new ListNode{0,head};
+        auto temp=head_pre;
+        int size=0;
+        while (temp->next!= nullptr)
+        {
+            temp=temp->next;
+            size++;
+        }
+        ListNode* temp1,*temp2;
+        swaptwoPairs_iter(head_pre,size, temp1,temp2);
+        auto result=head_pre->next;
+        delete head_pre;
+        return result;
+    }
+     void swaptwoPairs_iter(ListNode*&  head,int size,ListNode*& temp1,ListNode*&temp2) {
 
+        if(size>=4)
+        {
+            temp1=head->next;//1
+            head->next=temp1->next;//2
+            temp1->next=head->next->next->next;//3
+            temp2=head->next->next;//4
+            head->next->next=temp1;//5
+            temp2->next=temp1->next->next;//6
+            temp1->next->next=temp2;//7
+
+            size-=4;
+            swaptwoPairs_iter(head->next->next->next->next,size,temp1,temp2);
+        }
+        else
+        {
+            swapPairs_iter(head,size);
+        }
+    }
+    void swapPairs_iter(ListNode*&  head,int size) {
+        ListNode* temp;
+        if(size>=2)
+        {
+            temp=head->next;
+            head->next=temp->next;
+            temp->next=head->next->next;
+            head->next->next=temp;
+            size-=2;
+            swapPairs_iter(head->next->next,size);
+        }
     }
 };
-//leetcode submit region end(Prohibit modification and deletion)
 
+//leetcode submit region end(Prohibit modification and deletion)
 
 int main()
 {
    Solution s;
+
+    ListNode l1_node[4],l2_node[3];
+    ListNode * result;
+    l1_node[0].val=2;l1_node[0].next=&l1_node[1];
+    l1_node[1].val=4;l1_node[1].next=&l1_node[2];
+    l1_node[2].val=6;l1_node[2].next=&l1_node[3];
+    l1_node[3].val=5;l1_node[3].next= nullptr;
+    result=s.swapPairs(l1_node);
+    while(result!= nullptr)
+    {
+        cout<<result->val<<" ";
+        result=result->next;
+
+    }
 }
