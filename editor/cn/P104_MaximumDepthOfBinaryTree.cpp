f83bbb344b0fdf06fe423634d1ea28d4628be090
kylefjl <3219029834@mail.nwpu.edu.cn>
2024-03-18 15:59:59 +0800

    P104 二叉树的最大深度 P559 N 叉树的最大深度

---
+++ editor/cn/P104_MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,82 @@
+//给定一个二叉树 root ，返回其最大深度。 
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 10⁴] 区间内。 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1797 👎 0
+
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+/*
+* 104
+* @author fjl
+* @date 2024-03-18 15:35:30
+*/
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return getDepth(root);
+
+    }
+    int getDepth(TreeNode* node )
+    {
+        if(node==nullptr)return 0;
+        return 1+max(getDepth(node->left),getDepth(node->right));
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+   Solution s;
+
+}
---
+++ editor/cn/P559_MaximumDepthOfNAryTree.cpp
@@ -0,0 +1,106 @@
+//给定一个 N 叉树，找到其最大深度。 
+//
+// 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。 
+//
+// N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [1,null,3,2,4,null,5,6]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,
+//null,13,null,null,14]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树的深度不会超过 1000 。 
+// 树的节点数目位于 [0, 10⁴] 之间。 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 👍 385 👎 0
+
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+/*
+* N 叉树的最大深度
+* @author fjl
+* @date 2024-03-18 15:44:08
+*/
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        return getDepth(root);
+
+    }
+    int getDepth(Node* node )
+    {
+        if(node==nullptr)return 0;
+           int maxDepth = 0;
+        for (auto & node_i : node->children)
+        {
+            maxDepth= max(maxDepth,getDepth(node_i));
+        }
+        return 1+maxDepth;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+   Solution s;
+}
